4aad3a045c4d93372585b12dcbe22ecbd948bfdd
Eric Yon <eyon@Erics-MBP.lan>
2024-01-10 20:11:27 -0600

    full functionality and design

--- myscripts.js
+++ myscripts.js
@@ -6,9 +6,15 @@ function email() {
 }
 
 function validateEmail(email) {
+  const errmsg = document.getElementById("error-msg");
+  const errinput = document.getElementById("input");
+
   if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+    errmsg.classList.remove("show");
+    errinput.classList.remove("input-error");
     alert("Thank you for subscribing. You will be notified when we launch!");
   } else {
-    alert("Please enter a valid email.");
+    errmsg.classList.add("show");
+    errinput.classList.add("input-error");
   }
 }
